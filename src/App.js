edc6603cfced7bae203eec04111f47b83f9346b4
kanoun-foued <kanoun.foued10@gmail.com>
2020-12-04 23:36:29 +0100

    implement the infinite scrolling callback

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ListItem from "./components/ListItem";
 import Statistic from "./components/Statistic";
 import ItemCreator from "./components/ItemCreator";
+import {
+  fetchListItem,
+  removeItemAction,
+  addItemAction,
+} from "./store/actions/listItem";
 import "./App.css";
 
-let items = [];
-for (let i = 0; i < 1000; i++) {
-  items[i] = { id: i, text: Math.floor(Math.random() * 2000) };
-}
-
 export default function App() {
-  const [list, setList] = React.useState(items);
+  const dispatch = useDispatch();
+
+  // redux state
+  const page = useSelector((state) => state.listItems.page);
+  const hasMore = useSelector((state) => state.listItems.hasMore);
+  const listItems = useSelector((state) => state.listItems.listItem);
+  const isLoading = useSelector((state) => state.listItems.isLoading);
+  const error = useSelector((state) => state.listItems.error);
+  // redux state
+
   const [addedElements, setAddedElements] = React.useState(0);
   const [removedElements, setRemovedElements] = React.useState(0);
   const [item, setItem] = React.useState("");
-
   const [lastOperation, setLastOperation] = React.useState("none");
+
+  const observer = React.useRef();
+  const previousPage = React.useRef(-10);
+
+  React.useEffect(() => {
+    dispatch(fetchListItem(page));
+  }, []);
 
   const onItemChange = (e) => {
     setItem(e.target.value);
@@ -23,9 +39,7 @@ export default function App() {
 
   const deleteOnClick = React.useCallback(
     (id) => () => {
-      let index = list.findIndex((item) => item.id === id);
-      list.splice(index, 1);
-      setList([...list]);
+      dispatch(removeItemAction({ id }));
       setRemovedElements((prevState) => prevState + 1);
       setLastOperation("REMOVE");
     },
@@ -33,11 +47,39 @@ export default function App() {
   );
 
   const onAddItem = (item) => () => {
-    setList([...list, { id: Math.floor(Math.random() * 100000), text: item }]);
-    setAddedElements((prevState) => prevState + 1);
-    setItem("");
-    setLastOperation("ADD");
+    if (item) {
+      dispatch(
+        addItemAction({
+          item: { title: item, id: Math.floor(Math.random() * 10000) },
+        })
+      );
+      setAddedElements((prevState) => prevState + 1);
+      setItem("");
+      setLastOperation("ADD");
+    }
   };
+
+  const lastContentRef = React.useCallback(
+    (node) => {
+      if (isLoading || !hasMore) return;
+      if (observer.current) observer.current.disconnect();
+
+      observer.current = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting) {
+            if (previousPage.current === page) return;
+            previousPage.current = page;
+            dispatch(fetchListItem(page));
+          }
+        },
+        {
+          threshold: 1.0,
+        }
+      );
+      if (node) observer.current.observe(node);
+    },
+    [isLoading, hasMore, page]
+  );
 
   console.log("render App");
 
@@ -48,11 +90,27 @@ export default function App() {
         item={item}
         onAddItem={onAddItem(item)}
       />
-      <ListItem listOfItems={list} deleteOnClick={deleteOnClick} />
+
+      <div
+        style={{
+          height: 400,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <ListItem
+          lastContentRef={lastContentRef}
+          listOfItems={listItems}
+          deleteOnClick={deleteOnClick}
+        />
+        {isLoading ? <label>Loading ...</label> : null}
+        {error ? <label>{error}</label> : null}
+      </div>
+
       <Statistic
         addedElements={addedElements}
         removedElements={removedElements}
-        totalItems={list.length}
+        totalItems={listItems.length}
         lastOperation={lastOperation}
       />
     </div>
